6cbf5f8a576fa5deb2c00c53bee8cc043796dce0
xiong641 <59578368+xiong641@users.noreply.github.com>
2020-01-16 12:14:07 -0600

    Finished Team7 Strategy

--- team7.py
+++ team7.py
@@ -6,10 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'Team 7' # Only 10 chars displayed.
+strategy_name = 'Betray'
+strategy_description = 'Betray first, then based on oppenents last move, betray or collude'
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -26,13 +26,20 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    if len(my_history)==0:
+        return 'b'
+    elif my_history[-1]=='b' and their_history[-1]=='c':
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b'
+    else:
+        return 'b'
+                
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
+    Returns True or False, depending on whether result was as expected.
     '''
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
@@ -62,7 +69,7 @@ if __name__ == '__main__':
               # that is relevant to the test of move(). Here,
               # the simulation (if working correctly) would have awarded 
               # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
+              # move('bbb', 'ccc', 0, 0) returns 'bbb'.
               my_score=0, 
               their_score=0,
               result='b')             
